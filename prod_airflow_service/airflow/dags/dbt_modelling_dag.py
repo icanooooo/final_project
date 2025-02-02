9376807bf1a78772277919a6dda93aa4eb136d68
icanooo <muhihsan0@outlook.com>
2025-02-02 23:23:37 +0700

    create dbt commands for airflow

--- prod_airflow_service/airflow/dags/dbt_modelling_dag.py
+++ prod_airflow_service/airflow/dags/dbt_modelling_dag.py
@@ -2,4 +2,32 @@ from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
-dbt_source = "dbt run --profiles /opt/dbt/profiles"
+import yaml
+
+opening_command = "dbt run --select "
+middle_command = "--target "
+closing_command = " --profiles opt/dbt/profiles"
+
+def load_config():
+    with open("/opt/airflow/dags/configs/app_db.yaml", "r") as file:
+        return yaml.safe_load(file)
+    
+def get_commands(config):
+    commands = []
+
+    for name, values in config['models'].items():
+        models = ""
+
+        for table in config['models'][name]['tables']:
+            models += table['name'] + " "
+
+        cmd = opening_command + models + middle_command + config['models'][name]['target'] + closing_command
+
+        commands.append(cmd)
+        
+    sources = commands[0]
+    fct_dims = commands[1]
+    mart = commands[2]
+
+    return sources, fct_dims, mart
+
