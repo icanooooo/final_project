c01ae91882892bd037f35886ffe9bed130882b55
icanooo <muhihsan0@outlook.com>
2025-02-03 17:26:07 +0700

    succesfully dbt run source tables with airflow

--- prod_airflow_service/airflow/dags/dbt_modelling_dag.py
+++ prod_airflow_service/airflow/dags/dbt_modelling_dag.py
@@ -1,12 +1,15 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
-from datetime import datetime
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from helper.alert_helper import send_failure_to_discord
 
 import yaml
 
+
 opening_command = "dbt run --select "
 middle_command = "--target "
-closing_command = " --profiles opt/dbt/profiles"
+closing_command = " --profiles-dir profiles"
 
 def load_config():
     with open("/opt/airflow/dags/configs/app_db.yaml", "r") as file:
@@ -23,6 +26,8 @@ def get_commands(config):
 
         cmd = opening_command + models + middle_command + config['models'][name]['target'] + closing_command
 
+        cmd = "docker exec dbt-dbt-1 " + cmd + " --project-dir library_dbt"
+
         commands.append(cmd)
         
     sources = commands[0]
@@ -31,3 +36,32 @@ def get_commands(config):
 
     return sources, fct_dims, mart
 
+def create_dag():
+    config=load_config()
+    source_cmd, fct_dims_cmd, mart_cmd = get_commands(config)
+
+    default_args = {
+        'owner' : 'ican',
+        'retries' : 1,
+        'retry_delay' : timedelta(seconds=15),
+        'on_failure_callback' : send_failure_to_discord,
+    }
+
+    with DAG(
+        "dbt_modelling_dag",
+        start_date=datetime(2025, 2, 2),
+        tags=['nigquery_dags', 'dbt_dags'],
+        default_args=default_args,
+        schedule_interval='@daily',
+        catchup=False) as dag:
+
+        source_dag = BashOperator(
+            task_id='dbt_run_src',
+            bash_command=source_cmd
+        )
+
+        source_dag
+    
+    return dag
+
+globals()['library_dbt_modelling'] = create_dag()
